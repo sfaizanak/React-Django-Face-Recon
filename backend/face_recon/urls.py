d27875c83902b0b22f074e53d3bdec4886e005a7
sfaizanak <sfakfaizan38@gmail.com>
2024-10-29 19:51:39 +0530

    add files

--- backend/face_recon/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('register/organization_user/', OrganizationUserCreateView.as_view(), name='register_organization_user'),
-    path('register/employee_user/', EmployeeUserCreateView.as_view(), name='register_employee_user'),
-    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
-]
